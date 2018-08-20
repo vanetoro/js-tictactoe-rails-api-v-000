4533702967e64dfc1176ff72ce69769f765ab568
Vanessa Toro <vanetoro84@gmail.com>
2018-08-20 15:59:56 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,33 @@
 // Code your JavaScript / jQuery solution here
+let turn = 0;
+function player(turnCount){
+  if(turnCount%2 === 0){
+		return 'X'
+	} else{
+    return 'O'
+  }
+}
+
+function updateState(tdElement){
+  var move = player()
+  console.log(tdElement)
+  $(`${tdElement}`).text(move)
+  // $(`[data-x=${xCoord}][data-y=${yCoord}]`).text(move)
+}
+
+
+function setMessage(string){
+  $('#message').text(string)
+}
+
+function checkWinner(){
+
+  setMessage()
+}
+
+function doTurn(){
+  turn += 1
+  updateState()
+  checkWinner()
+
+}
