c6c46326bf54c90cc353a0ef18707711a1b6c22c
Vanessa Toro <vanetoro84@gmail.com>
2018-08-20 18:55:52 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,5 +1,17 @@
 // Code your JavaScript / jQuery solution here
-let turn = 0;
+var turn = 0;
+
+var winCombos = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+  ]
+
 function player(turnCount){
   if(turnCount%2 === 0){
 		return 'X'
